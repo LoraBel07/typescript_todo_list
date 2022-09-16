8640b7c8948efa81fe85857b8645cc80451cb3ca
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-09-16 19:35:03 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { TodoForm } from './components/TodoForm';
 
 function App() {
+  const [ todos, setTodos ] = useState<Array<Todo>>([]);
+
+  const addTodo: AddTodo = newTodo => {
+    if ( newTodo !== "") {
+      setTodos([...todos, {text: newTodo, complete: false}]);
+    }
+  }
   return (
     <div className="App">
       <h1>ToDo App</h1>
+      <TodoForm addTodo={addTodo}/>
     </div>
   );
 }
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface TodoFormProps {
+	addTodo: AddTodo;
+}
+
+export const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
+	const [ newTodo, setNewTodo ] = useState<string>("");
+
+	return (
+		<form className="todo-form">
+			<input type="text" value={newTodo} className="todo-input" placeholder='Add a task' />
+		</form> 
+		
+	)
+} 
