fd1bca44236e768f71e4cdda9719702c949c4750
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-09-16 21:03:59 -0400

    commit

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
 interface TodoFormProps {
 	addTodo: AddTodo;
@@ -7,9 +7,20 @@ interface TodoFormProps {
 export const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
 	const [ newTodo, setNewTodo ] = useState<string>("");
 
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewTodo(e.target.value);
+	}
+
+	const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+	}
+
 	return (
 		<form className="todo-form">
-			<input type="text" value={newTodo} className="todo-input" placeholder='Add a task' />
+			<input type="text" value={newTodo} className="todo-input" placeholder='Add a task' onChange={handleChange} />
+			<button type="submit" className='todo-button'>
+				Add a task
+			</button>
 		</form> 
 		
 	)
